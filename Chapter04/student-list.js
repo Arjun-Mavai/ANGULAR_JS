b98950c20e44ea278ee20d893ba81e3f4f27514d
Aman Mavai <amanmavai@gmail.com>
2016-04-28 09:45:13 +0000

    Adding Chapter04

---
+++ Chapter04/student-list.js
@@ -0,0 +1,134 @@
+angular.module('student').value('studentListValue', [
+  {
+    rollnumber: 6001,
+    firstname: 'Aman',
+    lastname: 'Mavai',
+    email: 'amanmavai@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6002,
+    firstname: 'Rohit',
+    lastname: 'Sharma',
+    email: 'rohitsharma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6003,
+    firstname: 'Manish',
+    lastname: 'Verma',
+    email: 'manishverma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6004,
+    firstname: 'Harish',
+    lastname: 'Verma',
+    email: 'harishverma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6005,
+    firstname: 'Srishti',
+    lastname: 'Saxena',
+    email: 'srsaxena@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6006,
+    firstname: 'Fyad',
+    lastname: 'Goyal',
+    email: 'fyadgoyal@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6007,
+    firstname: 'sonu',
+    lastname: 'ghosh',
+    email: 'sonughosh@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6008,
+    firstname: 'sujay',
+    lastname: 'prashant',
+    email: 'sujayprashant@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6009,
+    firstname: 'Purohan',
+    lastname: 'Shail',
+    email: 'purhoshail@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6010,
+    firstname: 'Arjun',
+    lastname: 'Mavai',
+    email: 'arjunmavai@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6011,
+    firstname: 'Arvind',
+    lastname: 'Kushwah',
+    email: 'kush.iiitm@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: 6012,
+    firstname: 'Yash',
+    lastname: 'Sharma',
+    email: 'yashsharma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  }
+]);
