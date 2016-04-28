7e9dc0539390673fcc9c557c4da06e86255a8bb5
Aman Mavai <amanmavai@gmail.com>
2016-04-28 06:33:18 +0000

    Creating Students Result Mangament Project in Chapter03

---
+++ Chapter03/student-list.js
@@ -0,0 +1,134 @@
+angular.module('student').value('studentListValue', [
+  {
+    rollnumber: '00001',
+    firstname: 'Aman',
+    lastname: 'Mavai',
+    email: 'amanmavai@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00002',
+    firstname: 'Rohit',
+    lastname: 'Sharma',
+    email: 'rohitsharma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00003',
+    firstname: 'Manish',
+    lastname: 'Verma',
+    email: 'manishverma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00004',
+    firstname: 'Harish',
+    lastname: 'Verma',
+    email: 'harishverma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00005',
+    firstname: 'Srishti',
+    lastname: 'Saxena',
+    email: 'srsaxena@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00006',
+    firstname: 'Fyad',
+    lastname: 'Goyal',
+    email: 'fyadgoyal@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00007',
+    firstname: 'sonu',
+    lastname: 'ghosh',
+    email: 'sonughosh@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00008',
+    firstname: 'sujay',
+    lastname: 'prashant',
+    email: 'sujayprashant@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '00009',
+    firstname: 'Purohan',
+    lastname: 'Shail',
+    email: 'purhoshail@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '000010',
+    firstname: 'Arjun',
+    lastname: 'Mavai',
+    email: 'arjunmavai@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '000011',
+    firstname: 'Arvind',
+    lastname: 'Kushwah',
+    email: 'kush.iiitm@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  },
+  {
+    rollnumber: '000012',
+    firstname: 'Yash',
+    lastname: 'Sharma',
+    email: 'yashsharma@gmail.com',
+    english: 78,
+    hindi: 88,
+    maths: 98,
+    science:42,
+    social: 75
+  }
+]);
---
+++ Chapter03/student.js
@@ -0,0 +1,112 @@
+//https://amanprojects-list.firebaseio.com/
+//https://ng-projects-list.firebaseio.com/
+angular.module('student', ['ngRoute', 'firebase'])
+
+.value('fbURL', 'https://aman-studentlist.firebaseio.com/')
+.service('fbRef', function(fbURL) {
+  return new Firebase(fbURL)
+})
+.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
+  var auth;
+  return function () {
+      if (auth) return $q.when(auth);
+      var authObj = $firebaseAuth(fbRef);
+      if (authObj.$getAuth()) {
+        return $q.when(auth = authObj.$getAuth());
+      }
+      var deferred = $q.defer();
+      authObj.$authAnonymously().then(function(authData) {
+          auth = authData;
+          deferred.resolve(authData);
+      });
+      return deferred.promise;
+  }
+})
+
+.service('Students', function($q, $firebase, fbRef, fbAuth, studentListValue) {
+  var self = this;
+  this.fetch = function () {
+    if (this.students) return $q.when(this.students);
+    return fbAuth().then(function(auth) {
+      var deferred = $q.defer();
+      var ref = fbRef.child('students-fresh/' + auth.auth.uid);
+      var $students = $firebase(ref);
+      ref.on('value', function(snapshot) {
+        if (snapshot.val() === null) {
+          $students.$set(studentListValue);
+        }
+        self.students = $students.$asArray();
+        deferred.resolve(self.students);
+      });
+
+      //Remove students list when no longer needed.
+      ref.onDisconnect().remove();
+      return deferred.promise;
+    });
+  };
+})
+
+.config(function($routeProvider) {
+  var resolveStudents = {
+    students: function (Students) {
+      return Students.fetch();
+    }
+  };
+
+  $routeProvider
+    .when('/', {
+      controller:'StudentListController as studentList',
+      templateUrl:'studentlist.html',
+      resolve: resolveStudents
+    })
+    .when('/edit/:studentId', {
+      controller:'EditStudentController as editStudent',
+      templateUrl:'detail.html',
+      resolve: resolveStudents
+    })
+    .when('/new', {
+      controller:'NewStudentController as editStudent',
+      templateUrl:'detail.html',
+      resolve: resolveStudents
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('StudentListController', function(students) {
+  var studentList = this;
+  studentList.students = students;
+})
+
+.controller('NewStudentController', function($location, students) {
+  var editStudent = this;
+  editStudent.save = function() {
+      students.$add(editStudent.student).then(function(data) {
+          $location.path('/');
+      });
+  };
+})
+
+.controller('EditStudentController',
+  function($location, $routeParams, students) {
+    var editStudent = this;
+    var studentId = $routeParams.studentId,
+        studentIndex;
+
+    editStudent.students = students;
+    studentIndex = editStudent.students.$indexFor(studentId);
+    editStudent.student = editStudent.students[studentIndex];
+
+    editStudent.destroy = function() {
+        editStudent.students.$remove(editStudent.student).then(function(data) {
+            $location.path('/');
+        });
+    };
+
+    editStudent.save = function() {
+        editStudent.students.$save(editStudent.student).then(function(data) {
+           $location.path('/');
+        });
+    };
+});
